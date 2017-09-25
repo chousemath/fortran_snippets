c11c8180a98f41053d1eac59d3d66d229774afa6
Joseph Sungpil Choi <chousemath@gmail.com>
2017-09-25 10:16:56 +0900

    just added another snippet

---
+++ writing.f95
@@ -0,0 +1,6 @@
+! This is a trivial program for writing to console
+program Writing
+  write (*, *) "Hello from Fortran!"
+  write (*, *) "This is another statement"
+  write (*, *) "Here is another, just for practice"
+end program Writing
